7732c3a6610b82dafa122e4932919c9f2d291b66
delitoast1 <delitoast1@gmail.com>
2025-11-09 20:22:38 +0800

    Added 3 new screens and updated HomeScreen with introductory texts

---
+++ Audio2Score/src/screens/LibraryScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const LibraryScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Library Screen</Text>
+  </View>
+);
